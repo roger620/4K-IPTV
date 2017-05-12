cdbaf099186ea31e093610fe0c26297f08a84b7e
ArronYin <diskerjtr@qq.com>
2017-05-12 23:39:58 +0800

    Update iptv72.sh

--- iptv72.sh
+++ iptv72.sh
@@ -20,19 +20,17 @@ echo
 
 
 #脚本提示
-echo 正在修改ss dnsmasq配置文件
-#移动到ss dnsmasq目录
-cd /koolshare/ss/rules
-#删除旧配置文件
+echo 正在增加IPTV开机自启动脚本
+#移动到脚本目录
+cd /jffs/scripts
+#删除之前的脚本
 echo 正在删除旧文件
-rm -rf dnsmasq.postconf
-#下载新配置文件
+rm -rf nat-start*
+#下载自启动脚本
 echo 正在下载新文件
-wget -q --no-check-certificate https://raw.githubusercontent.com/ArronYin/4K-IPTV/master/dnsmasq.conf
-#重命名新配置文件
-mv dnsmasq.conf dnsmasq.postconf
+wget -q --no-check-certificate https://raw.githubusercontent.com/ArronYin/4K-IPTV/master/nat-start
 #设置权限
-chmod -R 0755 dnsmasq.postconf
+chmod -R 0755 nat-start
 #完成提示
 echo 成功
 
@@ -41,12 +39,20 @@ ping 127.0.0.1 -c 2 > /dev/null
 #空行
 echo
 
-#运行提示
-echo 正在重启dnsmasq
-#重启dnsmasq
-service restart_dnsmasq > /dev/null
+#脚本提示
+echo 正添加IPTV运行脚本
+#移动到脚本目录
+cd /jffs/configs/dnsmasq.d
+#删除旧dnsmasq配置文件
+echo 正在删除旧文件
+rm -rf iptv.conf*
+#下载开机运行脚本
+echo 正在下载新文件
+wget -q --no-check-certificate https://raw.githubusercontent.com/ArronYin/4K-IPTV/master/iptv.conf
+#设置权限
+chmod -R 0644 iptv.conf
 #完成提示
-echo dnsmasq已重启
+echo 成功
 
 #延迟运行
 ping 127.0.0.1 -c 2 > /dev/null
@@ -58,5 +64,16 @@ cd
 
 #提示成功
 ping 127.0.0.1 -c 2 > /dev/null
-echo 脚本运行完成，如果光猫已经设置完毕，您可以把IPTV接在Lan1-4任意接口
+echo 脚本运行完成，如果光猫已经设置完毕，您可以把IPTV接在光猫lan口
+
+#延迟运行
 ping 127.0.0.1 -c 2 > /dev/null
+#空行
+echo
+
+#运行提示
+echo 正在重启路由器，请待路由器重启成功后，重新启动IPTV
+#延迟运行
+ping 127.0.0.1 -c 2 > /dev/null
+#重启路由器
+reboot > /dev/null
